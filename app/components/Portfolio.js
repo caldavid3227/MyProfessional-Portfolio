2bc0c96291a672a7d6433a12963cc0720f99c012
caldavi3227 <davidsoncaleb97@gmail.com>
2018-05-02 05:56:36 -0700

    portfolio page

--- app/components/Portfolio.js
+++ app/components/Portfolio.js
@@ -12,18 +12,20 @@ class Portfolio extends React.Component{
 					<hr/>
 				</div>
 				<div className="text-center">
-					The-PsuedoCoder
+					The-PsuedoCoder<br/>
+					<a href="https://the-psuedocoder.herokuapp.com/" className="thumbnail" target="blank"><img src="https://blog.nxp.com/wp-content/uploads/2017/10/Software-car-reuse-960x425.jpg" alt="The-PsuedoCoder"/></a>
+
 					<hr/>
 				</div>
-				<div className="row">	
-					<div className="grid-x">
-						<div className="cell medium-6">
-							Front-end Hangman
-						</div>			
-						<div className="cell medium-6">
-							Back-end Hangman
-						</div>	
-					</div>
+				<div className="grid-x">
+					<div className="cell small-6">
+						Church Finder<br/>
+						<a href="https://tranquil-atoll-37047.herokuapp.com/" className="thumbnail" target="blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhaDAEGG_EA9RS8qePPHBSANnVkr2Ouo1iRHTpFIGVfoFeO_-X" alt="ChurchFinder"/></a>
+					</div>			
+					<div className="cell auto">
+						Smart Bets<br/>
+						<a href="https://caldavid3227.github.io/Sports-Data-App/" className="thumbnail" target="blank"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0ZzLN-f1sI3FUvWYkM5A7CXsh8jfCSqiUJTg5Id3uz4H8BvyCkw" alt="SmartBets"/></a>
+					</div>	
 				</div>
 			</div>
 		);
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -4239,23 +4239,37 @@ var Portfolio = function (_get__$Component) {
 					'div',
 					{ className: 'text-center' },
 					'The-PsuedoCoder',
+					_react2.default.createElement('br', null),
+					_react2.default.createElement(
+						'a',
+						{ href: 'https://the-psuedocoder.herokuapp.com/', className: 'thumbnail', target: 'blank' },
+						_react2.default.createElement('img', { src: 'https://blog.nxp.com/wp-content/uploads/2017/10/Software-car-reuse-960x425.jpg', alt: 'The-PsuedoCoder' })
+					),
 					_react2.default.createElement('hr', null)
 				),
 				_react2.default.createElement(
 					'div',
-					{ className: 'row' },
+					{ className: 'grid-x' },
 					_react2.default.createElement(
 						'div',
-						{ className: 'grid-x' },
+						{ className: 'cell small-6' },
+						'Church Finder',
+						_react2.default.createElement('br', null),
 						_react2.default.createElement(
-							'div',
-							{ className: 'cell medium-6' },
-							'Front-end Hangman'
-						),
+							'a',
+							{ href: 'https://tranquil-atoll-37047.herokuapp.com/', className: 'thumbnail', target: 'blank' },
+							_react2.default.createElement('img', { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhaDAEGG_EA9RS8qePPHBSANnVkr2Ouo1iRHTpFIGVfoFeO_-X', alt: 'ChurchFinder' })
+						)
+					),
+					_react2.default.createElement(
+						'div',
+						{ className: 'cell auto' },
+						'Smart Bets',
+						_react2.default.createElement('br', null),
 						_react2.default.createElement(
-							'div',
-							{ className: 'cell medium-6' },
-							'Back-end Hangman'
+							'a',
+							{ href: 'https://caldavid3227.github.io/Sports-Data-App/', className: 'thumbnail', target: 'blank' },
+							_react2.default.createElement('img', { src: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0ZzLN-f1sI3FUvWYkM5A7CXsh8jfCSqiUJTg5Id3uz4H8BvyCkw', alt: 'SmartBets' })
 						)
 					)
 				)
