e8a040bfe91c6d9b96216224be70642fa18f2607
caldavi3227 <davidsoncaleb97@gmail.com>
2018-05-19 14:52:00 -0700

    about me update

--- app/components/AboutMe.js
+++ app/components/AboutMe.js
@@ -9,20 +9,19 @@ class About extends React.Component{
 		return(
 			<div>
 				<div>
-					<p> From the dawn of creation, man has strived to be better. 
-					Myself on the other hand, I strived to make unintelligent computers intelligent by speaking giberish to them...
-					</p>
-					<p>	No one knows when the first computer came into existence(1936), but since their inception, someone(Ada Lovelace) attempted to improve the way the computer, internet, and software run. I am proudly a member of this hard work and balderdash club known as web development.
-					 Like many who have come before me, I started my career in school and quickly grew to new hights of understanding, technology, and inovation! I have learned a great deal in such a short time; everything from front-end HTML to maddening back-end Node.js.
-					 In the days to come I hope to improve and expand my own site, <i>The-PsuedoCoder</i> and to learn as much as humanly possible about this world of computers. 
-					 Passion, a thirst for knowledge, and a healthy programming addiction drive me to the top, in hopes of being one of the best, and one of the few who earn the memory of changing and mondernizing how we write our code, one function at a time.<br/>
-					</p>
-					<p> Do you ever feel like somethings missing? Like your companies good, but it could be great? Something that brings a whole new level of fun, intellect, wit, and inovation?
-					 Well look no further for I am here! programming has become a very large and invaluable part of my life. It has taught me everything from creating and running my own local server, to valuable life lessons on being adatipble, intelligable, patient, and determained. 
-					 Not a day goes by where I find coding at the center of my next step forward. Yet for all of frustrations and strain this field brings, I always find myself smiling and contempt knowing that I have the oportunity to do soemthing few others can and that I have limitless potential at my ginger tips.
-					 We made it to the moon(unless we didn't actually go)on megabytes, imagine all we can do with the terabytes at our disposal each and every day, waiting and ready to make the indivdual, men, women, children, and the human race as a whole better, more intelligent, and more enlightened. 
-					 It is a great honor a privilage to be a part of this experience and to develope the skills necessary to push technology and the intert forward. The only limitations our programs have are the limitations we put on ourselves, so why would we let ourselves be limited when I can seak to learn more and more each passing moment?
-					 </p>  
+					<p>I strive to make unintelligent computers intelligent by speaking gibberish to them...
+No one knows when the first computer came into existence (in 1936), but since their inception, someone (Ada Lovelace) attempted to improve the way the computer, internet, and software run. I am a proud member of this hard work and balderdash club known as web development. Like many who have come before me, I started my career in school and quickly grew to new heights of understanding, technology, and innovation! I have learned a great deal in such a short time; everything from front-end HTML to maddening back-end Node.js. In the days to come I hope to improve and expand my own site, The-PsuedoCoder and to learn as much as humanly possible about this world of computers. Passion, a thirst for knowledge, and a healthy programming addiction drive me to the top, in hopes of being one of the best, and one of the few who earn the memory of changing and modernizing how we write our code, one function at a time.</p>
+					<p> Do you ever feel like somethings missing? 
+					Like your company is good  but it could be great? 
+					Something that brings a whole new level of fun, intellect, wit, and innovation? 
+					Well, look no further, for I am here! 
+					Programming has become a very large and invaluable part of my life. 
+					It has taught me everything from creating and running my own local server, to valuable life lessons on being adaptable, distinct, patient, and determined. 
+					Not a day goes by where I don’t find coding at the center of my next step forward. 
+					Yet for all the frustrations and strain this field brings, I always find myself smiling and content knowing that I have the opportunity to do something few others can and that I have limitless potential at my fingertips. 
+					We made it to the moon (unless we didn't actually go) on megabytes, imagine all we can do with the terabytes at our disposal each and every day. 
+					All that information, waiting and ready to make the individual men, women, children, and the human race as a whole better, more intelligent, and more enlightened. 
+					It is a great honor and privilege to be a part of this experience and to develop the skills necessary to push technology and the internet forward. The only limitations our applications have are the limitations we create.  Why would we let ourselves be limited when we can seek to learn more and more each passing moment?</p>  
 				</div>
 				<div>
 				 	<Link to="/Portfolio" className="button secondary">See my works here</Link> 
